ea89eee635177fe4d81b42938e1b0b9ecd3fdc53
Parth Mehrotra <Parth.mehrotra.cs@gmail.com>
2015-01-25 18:45:42 -0500

    ROS skelaton

---
+++ Experiemnts/Parth/ROS/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/lib:/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/lib/pkgconfig:/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/build"
+export PYTHONPATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/member/Documents/DriveAI/Experiemnts/Parth/ROS/src:$ROS_PACKAGE_PATH"
