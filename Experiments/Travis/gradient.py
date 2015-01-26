e98a4f4449ae4b080a14627dc08b6a5df3a14ce9
Travis <t.vanderstad@live.com>
2015-01-25 19:22:18 -0500

    Changed gradient to scipy-based implementation

--- Experiments/Travis/gradient.py
+++ Experiments/Travis/gradient.py
@@ -2,13 +2,13 @@ from numpy import *
 from PIL import Image
 from scipy.ndimage import filters
 image = 'shailene_woodley.png'
-im = array(Image.open(image))
+im = array(Image.open(image).convert('L'))
 
 imx = zeros(im.shape)
 filters.sobel(im,1,imx)
 imy = zeros(im.shape)
 filters.sobel(im,0,imy)
-magnitude = sqrt(imx**2+imy**2)
+grad= sqrt(imx**2+imy**2)
 
-out = Image.fromarray(uint8(im2))
+out = Image.fromarray(uint8(grad))
 out.save('output.png')
