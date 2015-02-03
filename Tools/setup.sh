ea35b3d7f93cf217e297aee2d90081db5cf67bf1
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-02-02 22:03:34 -0500

    updated install script

--- Tools/setup.sh
+++ Tools/setup.sh
@@ -1,31 +1,9 @@
-echo "installing virtualbox stuff"
-sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11
-echo "installing git"
-sudo apt-get install git
-echo "installing curl"
-sudo apt-get install  curl
-echo "installing tmux"
-sudo apt-get install tmux
-echo "installing vim"
-sudo apt-get install vim
-echo "installing pep8"
-sudo apt-get install pep8
-echo "installing python"
-sudo apt-get install python-dev
-echo "installing zsh"
-sudo apt-get install zsh
-echo "installing pv"
-sudo apt-get install pv
-echo "update"
+echo "installing virtualbox, git, curl, tmux, vim, pep8, python-dev, zsh, pv, java, checkinstall, openssh"
+sudo apt-get install virtualbox-guest-dkms virtualbox-guest-utils virtualbox-guest-x11 git curl tmux vim pep8 python-dev zsh pv openjdk-7-jdk openjdk-7-jre checkinstall openssh-server
 sudo apt-get update
 echo "oh my zsh"
 sudo curl -L http://install.ohmyz.sh | bash
-echo "java stuff"
-sudo apt-get install openjdk-7-jdk openjdk-7-jre
-echo "check install"
-sudo apt-get install checkinstall
-echo "ssh server"
-sudo apt-get install openssh-serverecho "ros stuff"
+ echo "ros stuff"
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu trusty main" > /etc/apt/sources.list.d/ros-latest.list'
 wget https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -O - | sudo apt-key add -
 sudo apt-get update
