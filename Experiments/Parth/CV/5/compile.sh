bf5fb8c783d25816f5fee547e1a24de063917382
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-02-03 01:16:04 -0500

    C++ opencv success

--- Experiments/Parth/CV/5/compile.sh
+++ Experiments/Parth/CV/5/compile.sh
@@ -1,1 +1,1 @@
-g++ -ggdb `pkg-config --cflags opencv` -o `basename opencvtest.cpp .cpp` opencvtest.cpp `pkg-config --libs opencv`
+g++ -ggdb `pkg-config --cflags opencv` -o `basename main.cpp .cpp` main.cpp `pkg-config --libs opencv`
