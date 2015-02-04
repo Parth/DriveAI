01c6cf8f2b84b3369e906371d6cbc16749d6f795
Travis Vanderstad <t.vanderstad@live.com>
2015-02-03 22:49:22 -0500

    good stuff going on in my experiments

--- Experiments/Travis/depth_matrix.py
+++ Experiments/Travis/depth_matrix.py
@@ -1,5 +1,6 @@
 from PIL import Image
 from numpy import *
+import math
 
 # Open the images
 left = array(Image.open('left.png'),'f');
@@ -11,6 +12,16 @@ leftRow = empty([1, left.shape[1], left.shape[2]])
 for i in range (0, left.shape[1]):
 	for j in range (0, left.shape[2]):
 		leftRow[0][i][j] = left[row][i][j]
+rightRow = empty([1, right.shape[1], right.shape[2]])
+for i in range (0, right.shape[1]):
+	for j in range (0, right.shape[2]):
+		rightRow[0][i][j] = right[row][i][j]
 
-out = Image.fromarray(uint8(leftRow))
-out.save('leftrow.png')
+# Calculate the values of the disparity matrix
+m = zeros([leftRow.shape[1], rightRow.shape[1]])
+for i in range(0,leftRow.shape[1]):
+	for j in range(0, rightRow.shape[1]):
+		m[i][j] = math.sqrt((leftRow[0][i][0]-rightRow[0][j][0])**2+(leftRow[0][i][1]-rightRow[0][j][1])**2+(leftRow[0][i][2]-rightRow[0][j][2])**2)
+
+out = Image.fromarray(uint8(m))
+out.save('matrix.png')
