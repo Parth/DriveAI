326c4b9109aa5d7a1f2ec5cebf6d4081ed669024
Travis Vanderstad <t.vanderstad@live.com>
2015-02-03 23:17:47 -0500

    added second scene matrix

--- Experiments/Travis/depth_matrix.py
+++ Experiments/Travis/depth_matrix.py
@@ -3,8 +3,8 @@ from numpy import *
 import math
 
 # Open the images
-left = array(Image.open('left.png'),'f');
-right = array(Image.open('right.png'),'f');
+left = array(Image.open('left2.png'),'f');
+right = array(Image.open('right2.png'),'f');
 
 # Retrieve the a row from each image (the same one)
 row = 256
@@ -24,4 +24,4 @@ for i in range(0,leftRow.shape[1]):
 		m[i][j] = math.sqrt((leftRow[0][i][0]-rightRow[0][j][0])**2+(leftRow[0][i][1]-rightRow[0][j][1])**2+(leftRow[0][i][2]-rightRow[0][j][2])**2)
 
 out = Image.fromarray(uint8(m))
-out.save('matrix.png')
+out.save('matrix2.png')
