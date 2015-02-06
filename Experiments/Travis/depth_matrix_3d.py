ed4419902473928127626f1ecffd75ab088972ab
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-02-06 13:12:52 -0500

    concurrent plotting

--- Experiments/Travis/depth_matrix_3d.py
+++ Experiments/Travis/depth_matrix_3d.py
@@ -18,9 +18,9 @@ for z in range(0, left.shape[0]):
 	print "calculating plane ", z+1, " of ", left.shape[0]
 	for rx in range(0, left.shape[1]):
 		for lx in range(0, right.shape[1]):
-			if(lx >= rx and random.random() < .000001):
-				m = math.sqrt((right[z][rx][0]-left[z][lx][0])**2 + (right[z][rx][1]-left[z][lx][1])**2 + (right[z][rx][2]-left[z][lx][2])**2)
-				if(m < random.random()*math.sqrt(3)):
-					numPoints = numPoints + 1
-					ax.scatter([rx],[lx],[z])
-pyplot.show()
+			#m = math.sqrt((right[z][rx][0]-left[z][lx][0])**2 + (right[z][rx][1]-left[z][lx][1])**2 + (right[z][rx][2]-left[z][lx][2])**2)
+			numPoints = numPoints + 1
+			#ax.scatter([rx],[lx],[z])
+			
+#pyplot.show()
+print numPoints
