f6e42951235f447de8d0ec3cacef5db620b163d7
Travis Vanderstad <t.vanderstad@live.com>
2015-03-14 23:31:07 -0400

    Test Cases & Cleanup
    Organized folder, replaced test cases (now including ground truth)

--- Experiments/Travis/gradient.py
+++ Experiments/Travis/gradient.py
@@ -1,7 +1,9 @@
+# Input: 'in.png'
+# Output: 'out.png' gradient magnitude of 'in.png'
 from numpy import *
 from PIL import Image
 from scipy.ndimage import filters
-image = 'shailene_woodley.png'
+image = 'in.png'
 im = array(Image.open(image).convert('L'))
 
 imx = zeros(im.shape)
@@ -11,4 +13,4 @@ filters.sobel(im,0,imy)
 grad= sqrt(imx**2+imy**2)
 
 out = Image.fromarray(uint8(grad))
-out.save('output.png')
+out.save('out.png')
