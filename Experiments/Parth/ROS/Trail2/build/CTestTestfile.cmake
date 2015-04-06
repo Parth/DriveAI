26d459a1875090c780061fd6c6776df3580c652d
Parth Mehrotra <parth.mehrotra.cs@gmail.com>
2015-04-05 23:30:21 -0400

    archived experiments

--- Experiments/Parth/ROS/Trail2/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/member/Documents/DriveAI/Experiments/Parth/ROS/Trail2/src
-# Build directory: /home/member/Documents/DriveAI/Experiments/Parth/ROS/Trail2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
-SUBDIRS(beginner_tutorials)
