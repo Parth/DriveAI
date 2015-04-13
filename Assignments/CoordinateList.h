b401b38eb7a3840b94d1a14a0a9c9d84d9523494
Travis Vanderstad <t.vanderstad@live.com>
2015-04-12 23:05:54 -0400

    Update CoordinateList.h

--- Assignments/CoordinateList.h
+++ Assignments/CoordinateList.h
@@ -1,21 +1,24 @@
 #define Coordinate double
-#define FOCAL_LENGTH .035
 
+// represents a list of the coordinates of 3d points, in one of several coordinate systems
 class CoordinateList {
     public:
         // List of all coordinate triples
         List<Coordinates[]> coordinates;
+        
         // Type of coordinates:
         // 0 = cartesian (x, y, z)
         // 1 = spherical (r, theta, roe)
         // 2 = perspective (u, v, w)
         char type;
+        
         /**
          * Converts the coordinate triples to the type specified using the same origin
          * 
          * @param type - the type to convert the coordinates to
          */
         void toType(int type);
+        
         /**
          * Converts the coodinates triples to the type specified using a new origin, offset from the previous origin by the given (x,y,z) triple
          * 
