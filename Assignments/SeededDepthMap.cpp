5ec1ae622b0a25f1c056e4d91aa06a5371cbcde3
Travis Vanderstad <t.vanderstad@live.com>
2015-04-12 23:29:10 -0400

    Update SeededDepthMap.cpp

--- Assignments/SeededDepthMap.cpp
+++ Assignments/SeededDepthMap.cpp
@@ -4,14 +4,33 @@ const double FOCAL_LENGTH = 0.035;
 const double c = 1000;
 const double k = 80;
 
+// coordinate triples that specify, in world (x,y,z) coordinates, the offset and orientation of the cameras relative to the lidar scanner
 const Coordinate[] rightCameraPosition;
 const Coordinate[] leftCameraPosition;
 const Coordinate[] rightCameraOrientation
 const Coordinate[] leftCameraOrientation;
 
-Coordinate[] lidarToWorld(Coordinate radius, Coordinate theta, Coordinate rho);
-Coordinate[] worldToImage(Coordinate x, Coordinate y, Coordinate z);
-Coordinate[] imageToWorld(Coordinate u, Coordinate v, Coordinate w);
-Coordinate[] imageToImage(Coordinate u, Coordinate v, Coordinate w);
+/**
+ * Conducts a stereo correspondence seeded by lidar data
+ * 
+ * @param left  left image to be used in correspondence
+ * @param right right image to be used in correspondence
+ * @param lidar lidar data to be used as the seed
+ */
+CoordinateList seededCorrespondence(Image left, Image right, CoordinateList lidar)
+{
+    CoordinateList leftList = seededCorrespondence(left, right);
+    CoordinateList rightList = seededCorrespondence(right, left);
+}
 
-
+/**
+ * Conducts a stereo correspondence seeded by lidar data
+ * 
+ * @param left  left image to be used in correspondence
+ * @param right right image to be used in correspondence
+ * @param lidar lidar data to be used as the seed
+ */
+CoordinateList seededCorrespondence(Image left, Image right, CoordinateList lidar)
+{
+    
+}
